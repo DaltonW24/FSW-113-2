d5a01e06c7fcfde8be7678f0f42715be70988cd4
DaltonW24 <dalton.welch@bryanuniversity.edu>
2022-08-20 11:38:00 -0400

    Almost done with staw wars

--- week3/assignments/starwars/starwars.js
+++ week3/assignments/starwars/starwars.js
@@ -4,6 +4,13 @@
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
 
+function fetchData(){
+    let queryType = document.getElementById("queryType").value
+    let itemID = document.getElementById("itemID").value
+    getFromSWAPI(queryType, itemID)
+}
+
+
 function getFromSWAPI(queryType, itemID) {
     // assign values to any necessary variables
     const fetchData = fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
@@ -14,22 +21,31 @@ function getFromSWAPI(queryType, itemID) {
         updateInfo(data)
     })
     .catch(function(err) {
-        console.warn(err)
+        console.warn(err) 
     })
 }
-function fetchData(queryType, itemID){
-    //i really have no clue what to do here
-    document.getElementById("ResponseData").innerHTML= queryType && itemID
-
-}
-
-
-
 // create a new function called 'updateInfo()' that receives the data from 
 // the call to that function (see above). Use logic to write the appropriate
 //labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
-function updateInfo(){
-    
-}
+
+function updateInfo(data){
+    let queryType = document.getElementById("queryType").value
+    console.log(data)
+    if(queryType === "people"){
+        document.getElementById("dataValue1").innerHTML = `Person Name: ${data.name}`
+        document.getElementById("dataValue2").innerHTML = `Hair Color: ${data.hair_color}`
+    }   
+        else if(queryType === "planets"){
+            document.getElementById("dataValue1").innerHTML = `Planet Name: ${data.name}`
+            document.getElementById("dataValue2").innerHTML = `Diameter: ${data.diameter}`
+        }
+            else if(queryType === "starships"){
+                document.getElementById("dataValue1").innerHTML = `Starship Name: ${data.name}`
+                document.getElementById("dataValue2").innerHTML = `Cost: ${data.cost_in_credits}`
+            }
+                else{
+                    console.log("Error: not found")
+                }
+ }
